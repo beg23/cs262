a7bfb1e80a7c1b4ee8eb47168da133c0a852d94f
beg23 <beg23@students.calvin.edu>
2019-09-20 21:22:05 -0400

    Add lab three

---
+++ Lab03/app/src/main/java/edu/calvin/cs262/beg23/lab03/MainActivity.java
@@ -0,0 +1,76 @@
+package edu.calvin.cs262.beg23.lab03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    /**
+     * Creates the main activity and assigns relevant text.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+    }
+
+    /**
+     * Launches the second activity when the `Send` button is pressed.
+     * <p>
+     * Logs that a button has been pressed.
+     * Sends an Intent with the message from the editText_main field.
+     * Waits for a result from the second activity.
+     * </p>
+     *
+     * @param view The current View object (the `Send` button).
+     */
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    /**
+     * Receives the result from the second activity.
+     * <p>
+     * Verifies that this is the correct result.
+     * Gets the reply text and displays it along with the header.
+     * </p>
+     *
+     * @param requestCode The int code of the request (should be TEXT).
+     * @param resultCode The int code of the result (should be OK).
+     * @param data The Intent sent by the second activity.
+     */
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+}
---
+++ Lab03/app/src/main/java/edu/calvin/cs262/beg23/lab03/SecondActivity.java
@@ -0,0 +1,42 @@
+package edu.calvin.cs262.beg23.lab03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
+
+    /**
+     * Creates the second activity and receives the Intent message from the main activity.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    /**
+     * Launches the main activity and sends a result Intent with the reply text to it.
+     *
+     * @param view The current View object (the `Reply` button).
+     */
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}
